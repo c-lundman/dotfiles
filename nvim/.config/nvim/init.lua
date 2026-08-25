7448e1c185b60d4467960b470515790a28adf9b6
Christoffer Lundman <christoffer.lundman@swedavia.se>
2026-08-25 16:30:17 +0200

    changed starship to pastel powerline theme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -887,6 +887,23 @@ require("lazy").setup({
     end,
   },
 
+  {
+    "e-ink-colorscheme/e-ink.nvim",
+    priority = 1000,
+    config = function()
+      require("e-ink").setup()
+      vim.cmd.colorscheme("e-ink")
+
+      -- choose light mode or dark mode
+      -- vim.opt.background = "dark"
+      vim.opt.background = "light"
+      --
+      -- or do
+      -- :set background=dark
+      -- :set background=light
+    end
+  },
+
   -- -- Onedark
   -- {
   --   "navarasu/onedark.nvim",
